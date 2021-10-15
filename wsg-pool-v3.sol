cb66480e08070c2ea49120d67125dd7c96ebb152
wsg-gg <82330202+wsg-gg@users.noreply.github.com>
2021-10-15 13:25:41 +0200

    Add files via upload

---
+++ wsg-pool-v3.sol
@@ -0,0 +1,231 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.6.12;
+
+import "./libs/pausable.sol";
+import "./libs/reentrancy-guard.sol";
+import "./libs/bep20.sol";
+import "./libs/safe-math.sol";
+
+contract WSGPoolV3 is ReentrancyGuard, Pausable {
+    using SafeMath for uint256;
+    using SafeBEP20 for IBEP20;
+
+    // STATE VARIABLES
+
+    IBEP20 public rewardsToken;
+    IBEP20 public stakingToken;
+    uint256 public periodFinish = 0;
+    uint256 public rewardRate = 0;
+    uint256 public rewardsDuration = 7890000; // 3 months
+    uint256 public lastUpdateTime;
+    uint256 public rewardPerTokenStored;
+
+    mapping(address => uint256) public userRewardPerTokenPaid;
+    mapping(address => uint256) public rewards;
+
+    uint256 private _totalSupply;
+    mapping(address => uint256) private _balances;
+
+    // CONSTRUCTOR
+
+    constructor(
+        address _rewardsToken,
+        address _stakingToken
+    ) public {
+        require(_rewardsToken != address(0) &&
+            _stakingToken != address(0), '!null');
+
+        rewardsToken = IBEP20(_rewardsToken);
+        stakingToken = IBEP20(_stakingToken);
+    }
+
+    // VIEWS
+
+    function totalSupply() external view returns (uint256) {
+        return _totalSupply;
+    }
+
+    function balanceOf(address account) external view returns (uint256) {
+        return _balances[account];
+    }
+
+    function lastTimeRewardApplicable() public view returns (uint256) {
+        return min(block.timestamp, periodFinish);
+    }
+
+    function rewardPerToken() public view returns (uint256) {
+        if (_totalSupply == 0) {
+            return rewardPerTokenStored;
+        }
+        return
+            rewardPerTokenStored.add(
+                lastTimeRewardApplicable()
+                    .sub(lastUpdateTime)
+                    .mul(rewardRate)
+                    .mul(1e18)
+                    .div(_totalSupply)
+            );
+    }
+
+    function earned(address account) public view returns (uint256) {
+        return
+            _balances[account]
+                .mul(rewardPerToken().sub(userRewardPerTokenPaid[account]))
+                .div(1e18)
+                .add(rewards[account]);
+    }
+
+    function getRewardForDuration() external view returns (uint256) {
+        return rewardRate.mul(rewardsDuration);
+    }
+
+    function min(uint256 a, uint256 b) public pure returns (uint256) {
+        return a < b ? a : b;
+    }
+
+    // PUBLIC FUNCTIONS
+
+    function stake(uint256 amount)
+        external
+        nonReentrant
+        notPaused
+        updateReward(msg.sender)
+    {
+        require(amount > 0, "Cannot stake 0");
+
+        uint256 balBefore = stakingToken.balanceOf(address(this));
+        stakingToken.safeTransferFrom(msg.sender, address(this), amount);
+        uint256 balAfter = stakingToken.balanceOf(address(this));
+        uint256 actualReceived = balAfter.sub(balBefore);
+
+        _totalSupply = _totalSupply.add(actualReceived);
+        _balances[msg.sender] = _balances[msg.sender].add(actualReceived);
+        
+        emit Staked(msg.sender, actualReceived);
+    }
+
+    function withdraw(uint256 amount)
+        public
+        nonReentrant
+        updateReward(msg.sender)
+    {
+        require(amount > 0, "Cannot withdraw 0");
+
+        _totalSupply = _totalSupply.sub(amount);
+        _balances[msg.sender] = _balances[msg.sender].sub(amount);
+        stakingToken.safeTransfer(msg.sender, amount);
+
+        emit Withdrawn(msg.sender, amount);
+    }
+
+    function getReward() public nonReentrant updateReward(msg.sender) {
+        uint256 reward = rewards[msg.sender];
+        if (reward > 0) {
+            rewards[msg.sender] = 0;
+            rewardsToken.safeTransfer(msg.sender, reward);
+            emit RewardPaid(msg.sender, reward);
+        }
+    }
+
+    function exit() external {
+        withdraw(_balances[msg.sender]);
+        
+        uint256 reward = rewards[msg.sender];
+        if (reward > 0) {
+            rewards[msg.sender] = 0;
+            rewardsToken.safeTransfer(msg.sender, reward);
+            emit RewardPaid(msg.sender, reward);
+        }
+    }
+
+    // RESTRICTED FUNCTIONS
+
+    function notifyRewardAmount(uint256 reward)
+        external
+        restricted
+        updateReward(address(0))
+    {
+        uint256 balBefore = rewardsToken.balanceOf(address(this));
+        rewardsToken.safeTransferFrom(msg.sender, address(this), reward);
+        uint256 balAfter = rewardsToken.balanceOf(address(this));
+        uint256 actualReceived = balAfter.sub(balBefore);
+        require(actualReceived == reward, "Whitelist the pool to exclude fees");
+
+        if (block.timestamp >= periodFinish) {
+            rewardRate = reward.div(rewardsDuration);
+        } else {
+            uint256 remaining = periodFinish.sub(block.timestamp);
+            uint256 leftover = remaining.mul(rewardRate);
+            rewardRate = reward.add(leftover).div(rewardsDuration);
+        }
+
+        // Ensure the provided reward amount is not more than the balance in the contract.
+        // This keeps the reward rate in the right range, preventing overflows due to
+        // very high values of rewardRate in the earned and rewardsPerToken functions;
+        // Reward + leftover must be less than 2^256 / 10^18 to avoid overflow.
+        uint256 balance = rewardsToken.balanceOf(address(this));
+        require(
+            rewardRate <= balance.div(rewardsDuration),
+            "Provided reward too high"
+        );
+
+        lastUpdateTime = block.timestamp;
+        periodFinish = block.timestamp.add(rewardsDuration);
+        emit RewardAdded(reward);
+    }
+
+    // Added to support recovering LP Rewards from other systems such as BAL to be distributed to holders
+    function recoverBEP20(address tokenAddress, uint256 tokenAmount)
+        external
+        onlyOwner
+    {
+        // Cannot recover the staking token or the rewards token
+        require(
+            tokenAddress != address(stakingToken) &&
+                tokenAddress != address(rewardsToken),
+            "Cannot withdraw the staking or rewards tokens"
+        );
+        IBEP20(tokenAddress).safeTransfer(owner(), tokenAmount);
+        emit Recovered(tokenAddress, tokenAmount);
+    }
+
+    function setRewardsDuration(uint256 _rewardsDuration) external restricted {
+        require(
+            block.timestamp > periodFinish,
+            "Previous rewards period must be complete before changing the duration for the new period"
+        );
+        rewardsDuration = _rewardsDuration;
+        emit RewardsDurationUpdated(rewardsDuration);
+    }
+
+    // *** MODIFIERS ***
+
+    modifier updateReward(address account) {
+        rewardPerTokenStored = rewardPerToken();
+        lastUpdateTime = lastTimeRewardApplicable();
+        if (account != address(0)) {
+            rewards[account] = earned(account);
+            userRewardPerTokenPaid[account] = rewardPerTokenStored;
+        }
+
+        _;
+    }
+
+    modifier restricted {
+        require(
+            msg.sender == owner(),
+            '!restricted'
+        );
+
+        _;
+    }
+
+    // EVENTS
+
+    event RewardAdded(uint256 reward);
+    event Staked(address indexed user, uint256 amount);
+    event Withdrawn(address indexed user, uint256 amount);
+    event RewardPaid(address indexed user, uint256 reward);
+    event RewardsDurationUpdated(uint256 newDuration);
+    event Recovered(address token, uint256 amount);
+}
